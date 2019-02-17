2ab29663f5cd542a621446bdb556e4aee08e702e
serzzhh <serzzhh@gmail>
2019-02-17 17:18:22 +0300

    module6-task1

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
   require_once 'init.php';
 
-  if (!$con) {
-  print("Ошибка подключения: " . mysqli_connect_error());
-  }
-  else {
-    require_once 'data.php';
-  }
-
-  $page_content = include_template('main.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
+  $page_content = include_template('main.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks, 'link' => $link]);
   $layout_content = include_template('layout.php', ['title' => "Дела в Порядке", 'content' => $page_content, 'projects' => $projects, 'tasks' => $tasks]);
   print($layout_content);
 ?>
