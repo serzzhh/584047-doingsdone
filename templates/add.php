4b77f80b4c461d07145e16b9e4f08ab215e77ffd
serzzhh <serzzhh@gmail>
2019-02-20 01:50:45 +0300

    module6-task2

---
+++ templates/add.php
@@ -0,0 +1,51 @@
+<h2 class="content__main-heading">Добавление задачи</h2>
+
+<form class="form"  action="" method="post" enctype="multipart/form-data">
+  <div class="form__row">
+    <?php $classname = isset($errors['name']) ? "form__input--error" : "";
+        $value = isset($task['name']) ? $task['name'] : ""; ?>
+    <label class="form__label" for="name">Название <sup>*</sup></label>
+
+    <input class="form__input <?=$classname;?>" type="text" name="name" id="name" value="<?=$value;?>" placeholder="Введите название">
+    <?php if (isset($errors['name'])): ?>
+    <p class="form__message"><?=$errors['name'] ?></p>
+    <?php endif; ?>
+  </div>
+
+  <div class="form__row">
+    <label class="form__label" for="project">Проект</label>
+
+    <select class="form__input form__input--select" name="project" id="project">
+      <?php foreach ($projects as $project): ?>
+      <option value="<?=$project["id"] ?>"><?=$project["name"] ?></option>
+      <?php endforeach; ?>
+    </select>
+  </div>
+
+  <div class="form__row">
+    <?php $classname = isset($errors['date']) ? "form__input--error" : "";
+        $value = isset($task['date']) ? $task['date'] : ""; ?>
+    <label class="form__label" for="date">Дата выполнения</label>
+
+    <input class="form__input form__input--date <?=$classname;?>" type="date" name="date" id="date" value="<?=$value;?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+    <?php if (isset($errors['date'])): ?>
+    <p class="form__message"><?=$errors['date'] ?></p>
+    <?php endif; ?>
+  </div>
+
+  <div class="form__row">
+    <label class="form__label" for="preview">Файл</label>
+
+    <div class="form__input-file">
+      <input class="visually-hidden" type="file" name="preview" id="preview" value="">
+
+      <label class="button button--transparent" for="preview">
+        <span>Выберите файл</span>
+      </label>
+    </div>
+  </div>
+
+  <div class="form__row form__row--controls">
+    <input class="button" type="submit" name="" value="Добавить">
+  </div>
+</form>
--- templates/main.php
+++ templates/main.php
@@ -1,34 +1,27 @@
 <h2 class="content__main-heading">Список задач</h2>
 
 <form class="search-form" action="index.php" method="post">
-    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+  <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
 
-    <input class="search-form__submit" type="submit" name="" value="Искать">
+  <input class="search-form__submit" type="submit" name="" value="Искать">
 </form>
 
 <div class="tasks-controls">
     <nav class="tasks-switch">
-        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-        <a href="/" class="tasks-switch__item">Повестка дня</a>
-        <a href="/" class="tasks-switch__item">Завтра</a>
-        <a href="/" class="tasks-switch__item">Просроченные</a>
+      <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+      <a href="/" class="tasks-switch__item">Повестка дня</a>
+      <a href="/" class="tasks-switch__item">Завтра</a>
+      <a href="/" class="tasks-switch__item">Просроченные</a>
     </nav>
 
     <label class="checkbox">
-        <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks): ?>checked<?php endif; ?>>
-        <span class="checkbox__text">Показывать выполненные</span>
+      <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
+      <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks): ?>checked<?php endif; ?>>
+      <span class="checkbox__text">Показывать выполненные</span>
     </label>
 </div>
 
 <table class="tasks">
-  <?php if (isset($_GET["project_id"])): ?>
-   <?php $id_count = get_res($link, "SELECT COUNT(*) AS count FROM tasks WHERE id_project = ?", $_GET["project_id"]);
-    if ($id_count[0]["count"] < 1): ?>
-      <p>ошибка 404</p>
-      <?php http_response_code(404); ?>
-    <?php endif; ?>
-  <?php endif; ?>
   <?php if (http_response_code() === 200): ?>
     <?php foreach ($tasks as $key => $value): ?>
       <?php if ($show_complete_tasks || !$value["completed"]): ?>
@@ -42,13 +35,15 @@
             </td>
 
             <td class="task__file">
-                <a class="download-link" href="#">Home.psd</a>
+                <a class="download-link" href="<?=$value["file"];?>"><?=$value["file"];?></a>
             </td>
 
-            <td class="task__date"><?=$value["deadline"];?></td>
+            <td class="task__date"><?=isset($value["deadline"]) ? date("d.m.Y", strtotime($value["deadline"])) : "";?></td>
         </tr>
         <?php endif; ?>
       <?php endif; ?>
     <?php endforeach; ?>
+  <?php else: ?>
+    <p>Ошибка 404</p>
   <?php endif; ?>
 </table>
